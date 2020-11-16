b22a4a4a362afa08e7c208810488a1bff955ce62
Touch <s.chantouch@mango-byte.com>
2020-11-16 22:16:41 +0700

    chore(fixed): :bug: fixed error

--- src/VuePlayr.vue
+++ src/VuePlayr.vue
@@ -1,15 +1,27 @@
+<template>
+  <div class="vue--playr"><slot /></div>
+</template>
+
 <script>
 import Plyr from 'plyr'
 import 'plyr/dist/plyr.css'
 
 export default {
-  name: 'VuePlyr',
+  name: 'VuePlayr',
   props: {
     options: {
       type: Object,
       required: false,
       default () {
         return {}
+      }
+    },
+    /** Array of events to emit from the plyr object **/
+    emit: {
+      type: Array,
+      required: false,
+      default () {
+        return []
       }
     }
   },
@@ -21,38 +33,33 @@ export default {
   computed: {
     opts () {
       const options = this.options
-      if (
-        !Object.prototype.hasOwnProperty.call(
-          this.options,
-          'hideYouTubeDOMError'
-        )
-      ) {
+      // eslint-disable-next-line no-prototype-builtins
+      if (!this.options.hasOwnProperty('hideYouTubeDOMError')) {
         options.hideYouTubeDOMError = true
       }
       return options
     }
   },
   mounted () {
-    this.player = new Plyr(this.$el, this.opts)
+    this.player = new Plyr(this.$el.firstChild, this.opts)
+    this.emit.forEach(element => {
+      this.player.on(element, this.emitPlayerEvent)
+    })
   },
-  beforeUnmount () {
+  beforeDestroy () {
     try {
       this.player.destroy()
     } catch (e) {
-      if (
-        !(
-          this.opts.hideYouTubeDOMError &&
-          e.message === 'The YouTube player is not attached to the DOM.'
-        )
-      ) {
+      if (!(this.opts.hideYouTubeDOMError && e.message === 'The YouTube player is not attached to the DOM.')) {
         // eslint-disable-next-line no-console
         console.error(e)
       }
     }
   },
-  render () {
-    const slots = this.$slots.default
-    return typeof slots === 'function' ? slots()[0] : slots
+  methods: {
+    emitPlayerEvent (event) {
+      this.$emit(event.type, event)
+    }
   }
 }
 </script>
